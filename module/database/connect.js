c6b289c32b70f75a17d11ee20160a2681281ac1c
ArdiSasongko <ardisasongko002@gmail.com>
2023-07-06 21:13:19 +0700

    update connect js

--- module/database/connect.js
+++ module/database/connect.js
@@ -1,17 +1,34 @@
-const { Sequelize } = require("sequelize")
-const  { SecretManagerServiceClient } = require("@google-cloud/secret-manager")
+const { Sequelize } = require("sequelize");
+const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
 
-const Client = SecretManagerServiceClient()
-const [version] = await Client.accessSecretVersion({
-    name: `projects/885281433299/secrets/user_db/versions/1`
-})
-const payload = version.payload.data.toString()
-const secret = JSON.parse(payload)
+async function getSecret() {
+  const client = new SecretManagerServiceClient();
+  const [version] = await client.accessSecretVersion({
+    name: `projects/885281433299/secrets/user_db/versions/1`,
+  });
+  const payload = version.payload.data.toString();
+  const secret = JSON.parse(payload);
+  return secret;
+}
 
-const sequelize = new Sequelize("db_latihan", secret, " ",{
-    host : "127.0.0.1",
-    port : "3306",
-    dialect : "mysql"
-})
+async function connectToDatabase() {
+  const secret = await getSecret();
 
-module.exports = sequelize
+  const sequelize = new Sequelize("db_latihan", secret.username, "", {
+    host: "127.0.0.1",
+    port: "3306",
+    dialect: "mysql",
+  });
+
+  try {
+    await sequelize.authenticate();
+    console.log("Connection to the database has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+module.exports = {
+  sequelize,
+  connectToDatabase
+};
